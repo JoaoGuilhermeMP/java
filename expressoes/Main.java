3d41dfb7183187e9cd1cdae6334be5ddca85e97b
EoRedneVida <joaoguilhermempdev@gmail.com>
2024-08-12 17:16:24 -0300

    Initial commit

---
+++ expressoes/Main.java
@@ -0,0 +1,11 @@
+package expressoes;
+
+public class Main {
+    public static void main(String[] args) {
+        int friends = 10;
+
+        friends++;
+
+        System.out.println(friends);
+    }
+}
---
+++ teste scanner/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual o seu nome? ");
+        String name = scanner.nextLine();
+        System.out.println("Quantos anos você tem? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Qual sua comida preferida? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Olá "+name);
+        System.out.println("Você tem " +age+ " anos.");
+        System.out.println("Sua comida preferida é "+food);
+
+    }
+
+}
